bcc176f42fedb309bac510881f424086fee73d6e
oni193 <keykeykey193@gmail.com>
2020-12-10 16:35:57 +0900

    :dizzy: Hook step 1 commit

--- src/index.js
+++ src/index.js
@@ -1,64 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+//import styled from 'styled-components';
 
-class Square extends React.Component {
-    render() {
-      return (
-        <button className="square">
-          {this.props.value}
-        </button>
-      );
-    }
+
+
+const Game = () => {
+  return(
+    <div className="game">
+      <div className="game-board">
+        <Board/>
+      </div>
+      <div className="game-info">
+        <div>{/* status */}</div>
+        <div>{/* TODO */}</div>
+      </div>
+    </div>
+  );
+}
+
+
+const Board = () => {
+  const renderSquare = (i) => {
+    return <Square value={i} />;
   }
-  
-  class Board extends React.Component {
-    renderSquare(i) {
-      return <Square value={i} />;
-    }
-  
-    render() {
-      const status = 'Next player: X';
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
-    }
-  }
-  
-  class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
-    }
-  }
-  
+  const status = 'Next player : X';
+    
+  return(
+    <div className="boardContainer">
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>    
+  );
+}
+
+
+export const Square = ({value}) => {
+  return(
+    <div className="square">
+      {value}
+    </div>
+  );
+}
+
+
   // ========================================
   
   ReactDOM.render(
